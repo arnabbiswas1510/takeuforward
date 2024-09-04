e054406b5ed53cd7fad5b2579374516cd3d41858
arnabbiswas <arnabbiswas@yahoo.com>
2024-09-04 19:20:49 -0400

    Trees m106

--- Tree/m106-ConstructBinaryTreefromInorderandPostorderTraversal.py
+++ Tree/m106-ConstructBinaryTreefromInorderandPostorderTraversal.py
@@ -1,3 +1,27 @@
 """
+Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is
+the postorder traversal of the same tree, construct and return the binary tree.
 
+Remember the method definitions and that you need an external hashMap
 """
+from treeOperations import Node, Tree
+
+def buildTree(inorder, postorder):
+    def helper(inLeft, inRight):
+        root=None
+        #Dont forget terminal condition
+        if not postorder or inLeft > inRight:
+            return None
+        val = postorder.pop()
+        root=Node(val)
+        index=inMap[val] #Required to do this in this step
+        # If done in the 2 lines below will cause bug since its in recursive context
+        root.left=helper(inLeft,index-1) #Remember to use inLeft and inRight here as terminal indexes and not 0, len
+        root.right=helper(index+1,inRight)
+        return root
+    #Note that it's a map of val:idx
+    inMap={val:idx for idx, val in enumerate(inorder)}
+    return helper(0,len(inorder))
+
+t=Tree()
+t.printLevelOrder(buildTree([9,3,15,20,7], [9,15,7,20,3]))
---
+++ Tree/m208-ImplementTrie.py
@@ -0,0 +1,4 @@
+"""
+In a Trie you can have upto 26 children
+Each node has a single char value and may also have some extra value (like end of word)
+"""
